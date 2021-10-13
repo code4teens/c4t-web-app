8d93124408572450e89b7f45c1e511d749619d8c
Hans Hazairi <hanshazairi@gmail.com>
2021-10-13 17:31:30 +0800

    chore: handle login errors

--- auth/auth.py
+++ auth/auth.py
@@ -1,8 +1,32 @@
-from flask import Blueprint, render_template
+import os
+
+from flask import Blueprint, flash, render_template, request
+import requests
 
 auth = Blueprint('auth', __name__, template_folder='templates/auth')
 
 
 @auth.route('/login')
-def login():
+def login_get():
+    return render_template('login.html')
+
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    id = request.form.get('id')
+    password = request.form.get('password')
+
+    api = os.environ.get('API_URL')
+    url = f'{api}/users/{id}/login'
+    r = requests.post(url, json={'password': password})
+
+    if r.status_code == 200:
+        pass
+    elif r.status_code == 401:
+        flash('Invalid password.')
+    elif r.status_code == 404:
+        flash('Invalid username.')
+    else:
+        flash('An error occurred.')
+
     return render_template('login.html')
