42b5095f93dd7f32d4db115ae9fd2fff33386635
Hans Hazairi <hanshazairi@gmail.com>
2021-10-14 14:51:25 +0800

    feat: add first-time login page

--- auth/auth.py
+++ auth/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 import bcrypt
 
+from database import db_session
 from models import User
 
 auth = Blueprint('auth', __name__, template_folder='templates/auth')
@@ -22,19 +23,51 @@ def login_post():
     user = User.query.filter_by(id=id).one_or_none()
 
     if user is None:
-        flash('Invalid ID')
-    else:
-        if bcrypt.checkpw(
-            password.encode('utf-8'),
-            user.password.encode('utf-8')
-        ):
-            login_user(user)
+        flash('ID does not exist')
+    elif user.password is None:
+        flash('Please click first-time login')
+    elif bcrypt.checkpw(
+        password.encode('utf-8'), user.password.encode('utf-8')
+    ):
+        login_user(user)
 
-            return redirect(url_for('user.profile', id=current_user.id))
-        else:
-            flash('Invalid password')
+        return redirect(url_for('user.profile', id=current_user.id))
+    else:
+        flash('Wrong password')
 
     return render_template('login.html')
+
+
+@auth.route('/first_login')
+def first_login_get():
+    return render_template('first_login.html')
+
+
+@auth.route('/first_login', methods=['POST'])
+def first_login_post():
+    id = request.form.get('id')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm-password')
+    user = User.query.filter_by(id=id).one_or_none()
+
+    if user is None:
+        flash('ID does not exist')
+    elif user.password is not None:
+        flash('You already set a password')
+    elif password is None or confirm_password is None \
+            or password != confirm_password:
+        flash('Passwords do not match')
+    else:
+        user.password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt()
+        )
+        db_session.merge(user)
+        db_session.commit()
+        login_user(user)
+
+        return redirect(url_for('user.profile', id=current_user.id))
+
+    return render_template('first_login.html')
 
 
 @auth.route('/logout')
