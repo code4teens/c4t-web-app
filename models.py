990b423fa7c8b0f3059f20b7eb0816e0be905f8b
Hans Hazairi <hanshazairi@gmail.com>
2021-10-15 17:37:52 +0800

    chore: show cohort name in profile

--- models.py
+++ models.py
@@ -4,10 +4,12 @@ from sqlalchemy import (
     Boolean,
     Column,
     DateTime,
+    ForeignKey,
     func,
     SmallInteger,
     String
 )
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -19,7 +21,19 @@ class User(UserMixin, Base):
     name = Column(String(64), nullable=False)
     discriminator = Column(String(4), nullable=False)
     display_name = Column(String(64), nullable=False)
-    cohort_id = Column(SmallInteger, nullable=True)
+    cohort_id = Column(SmallInteger, ForeignKey('cohort.id'), nullable=True)
     is_admin = Column(Boolean, nullable=False, default=False)
     created_at = Column(DateTime, nullable=False, default=func.now())
     last_updated = Column(DateTime, nullable=False, default=func.now())
+
+    cohort = relationship('Cohort', back_populates='user')
+
+
+class Cohort(Base):
+    __tablename__ = 'cohort'
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(64), nullable=False)
+    duration = Column(SmallInteger, nullable=False)
+    start_date = Column(DateTime, nullable=False)
+
+    user = relationship('User', back_populates='cohort')
