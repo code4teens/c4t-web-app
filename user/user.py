4dcaf9fbac725f7e0397c31a56012525e95f59c2
Hans Hazairi <hanshazairi@gmail.com>
2021-10-20 15:16:29 +0800

    chore: add new dashboard template

--- user/user.py
+++ user/user.py
@@ -16,12 +16,22 @@ def profile(id):
 
     return render_template('profile.html', user=user)
 
-@user.route('/dashboard', methods=['GET', 'POST'])
+
+@user.route('/dashboard')
 @login_required
-def dashboard_page():
-    print(dir(current_user))
+def dashboard():
     user = User.query.filter_by(name=current_user.name)
     if user is None:
         abort(404)
 
     return render_template('dashboard.html', user=user)
+
+
+@user.route('/other_dashboard')
+@login_required
+def other_dashboard():
+    user = User.query.filter_by(name=current_user.name)
+    if user is None:
+        abort(404)
+
+    return render_template('other_dashboard.html', user=user)
