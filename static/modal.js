743505534b2079c7c57ddc8106b9c2af78e421eb
Hans Hazairi <hanshazairi@gmail.com>
2021-11-04 01:33:32 +0800

    chore: refactor & housekeeping

--- static/modal.js
+++ static/modal.js
@@ -6,10 +6,14 @@ function subscribe() {
     success: function () {
       $(".modal-header").children("h5").text("Success!");
       $(".modal-body").children("p").text("Thank you for subscribing!");
+      $("#modal-button").removeClass("btn-danger");
+      $("#modal-button").addClass("btn-success");
     },
     error: function () {
-      $(".modal-header").children("h5").text("Ooooooops!");
+      $(".modal-header").children("h5").text("Oops!");
       $(".modal-body").children("p").text("Invalid email!");
+      $("#modal-button").removeClass("btn-success");
+      $("#modal-button").addClass("btn-danger");
     },
     complete: function () {
       $(".modal").modal("show");
